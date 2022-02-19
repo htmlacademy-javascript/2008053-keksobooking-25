3ef6d8191a64e931930fe28347252a07b06ef717
triple-sun <sploit.strannik@gmail.com>
2022-02-19 22:53:46 +0300

    Добавлена проверка на отсутствие параметров при вызове функций

--- js/main.js
+++ js/main.js
@@ -1,29 +1,35 @@
 //Функция взята со StackOverflow: https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
 function getRandomInteger(min, max) {
+  //Проверяем с помощью isNaN т.к. !argument срабатывает в т.ч. если argument = 0
+  if (isNaN(min * max)) {
+    throw new Error('Укажите значения "от" и "до"');
+  }
   if (min < 0) {
-    return alert('Значение "от" не должно быть меньше нуля');
+    throw new Error('Значение "от" не должно быть меньше нуля');
   }
 
   if (min >= max) {
-    return alert('Значение "от" должно быть меньше, чем "до"');
+    throw new Error('Значение "от" должно быть меньше, чем "до"');
   }
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-getRandomInteger();
-
 //Функция взята со StackOverflow: https://stackoverflow.com/questions/17726753/get-a-random-number-between-0-0200-and-0-120-float-numbers
 function getRandomCoordinates(min, max, decimal) {
+  if (isNaN(min * max * decimal)) {
+    throw new Error('Укажите значения "от", "до" и количество знаков после запятой');
+  }
   if (min < 0) {
-    return alert('Значение "от" не должно быть меньше нуля');
+    throw new Error('Значение "от" не должно быть меньше нуля');
   }
 
   if (min >= max) {
-    return alert('Значение "от" должно быть меньше, чем "до"');
+    throw new Error('Значение "от" должно быть меньше, чем "до"');
   }
 
   return (Math.random() * (max - min) + min).toFixed(decimal);
 }
 
+getRandomInteger();
 getRandomCoordinates();
