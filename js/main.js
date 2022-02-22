4d4136ff8d5c3a732d4a8afd4fa9437532fc8600
triple-sun <sploit.strannik@gmail.com>
2022-02-22 19:03:02 +0300

    Исправлена метод проверки наличия аргументов

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 //Функция взята со StackOverflow: https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
 function getRandomInteger(min, max) {
-  //Проверяем с помощью isNaN т.к. !argument срабатывает в т.ч. если argument = 0
-  if (isNaN(min * max)) {
+  if (min !== 0 && !min || !max) {
     throw new Error('Укажите значения "от" и "до"');
   }
   if (min < 0) {
@@ -17,13 +16,12 @@ function getRandomInteger(min, max) {
 
 //Функция взята со StackOverflow: https://stackoverflow.com/questions/17726753/get-a-random-number-between-0-0200-and-0-120-float-numbers
 function getRandomCoordinates(min, max, decimal) {
-  if (isNaN(min * max * decimal)) {
+  if (min !== 0 && !min || !max || !decimal) {
     throw new Error('Укажите значения "от", "до" и количество знаков после запятой');
   }
   if (min < 0) {
     throw new Error('Значение "от" не должно быть меньше нуля');
   }
-
   if (min >= max) {
     throw new Error('Значение "от" должно быть меньше, чем "до"');
   }
