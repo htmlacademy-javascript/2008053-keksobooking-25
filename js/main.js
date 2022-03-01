93f6145cf1e896ce1a303eae38769fedff4d4668
triple-sun <sploit.strannik@gmail.com>
2022-03-01 20:56:46 +0300

    Organized file structure

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+const PLACE_TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const CHECK_TIMES = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+];
+
 //Функция взята со StackOverflow: https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
 function getRandomInteger(min, max) {
   if (min !== 0 && !min || !max) {
@@ -29,12 +38,8 @@ function getRandomFloat(min, max, decimal) {
   return (Math.random() * (max - min) + min).toFixed(decimal);
 }
 
-getRandomInteger(1, 2);
-getRandomFloat(0, 2, 1);
-
-
-const usedNumbers = [];
 function getAvatarNumber() {
+  const usedNumbers = [];
   const imgNumber = Number(getRandomInteger(1, 10));
   if (!usedNumbers.includes(imgNumber)) {
     usedNumbers.push(imgNumber);
@@ -42,9 +47,8 @@ function getAvatarNumber() {
       return `0${  imgNumber}`;
     }
     return imgNumber;
-  } else {
-    getAvatarNumber();
   }
+  getAvatarNumber();
 }
 
 function getAvatar() {
@@ -53,18 +57,10 @@ function getAvatar() {
   };
 }
 
-const PLACE_TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const CHECK_TIMES = ['12:00', '13:00', '14:00'];
-const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+
 function getFeatures() {
   return FEATURES.filter((feature) => feature.length > getRandomInteger(3, 11));
 }
-
-const PHOTOS = [
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
-];
 
 function getPhotos() {
   PHOTOS.length = getRandomInteger(1, 3);
