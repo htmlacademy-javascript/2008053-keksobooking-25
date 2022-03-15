e66f4af3a67994021270167dcec8d6811ef03940
triple-sun <sploit.strannik@gmail.com>
2022-03-15 16:45:11 +0300

    Page mode switching and util.js cleanup

--- js/util.js
+++ js/util.js
@@ -1,32 +1,38 @@
-//Функция взята со StackOverflow: https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
-const getRandomInteger = (min, max) => {
-  if (min !== 0 && !min || !max) {
-    throw new Error('Укажите значения "от" и "до"');
-  }
-  if (min < 0) {
-    throw new Error('Значение "от" не должно быть меньше нуля');
-  }
+let winX, winY;
 
-  if (min >= max) {
-    throw new Error('Значение "от" должно быть меньше, чем "до"');
-  }
+//Функция взята со StackOverflow: https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range и доработана
+const getRandomInteger = (a, b) => {
+  const min = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const max = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-//Функция взята со StackOverflow: https://stackoverflow.com/questions/17726753/get-a-random-number-between-0-0200-and-0-120-float-numbers
-const getRandomFloat = (min, max, decimal) => {
-  if (min !== 0 && !min || !max || !decimal) {
-    throw new Error('Укажите значения "от", "до" и количество знаков после запятой');
-  }
-  if (min < 0) {
-    throw new Error('Значение "от" не должно быть меньше нуля');
-  }
-  if (min >= max) {
-    throw new Error('Значение "от" должно быть меньше, чем "до"');
-  }
+//Функция взята со StackOverflow: https://stackoverflow.com/questions/17726753/get-a-random-number-between-0-0200-and-0-120-float-numbers и доработана
+const getRandomFloat = (a, b, decimal = 1) => {
+  const min = Math.min(Math.abs(a), Math.abs(b));
+  const max = Math.max(Math.abs(a), Math.abs(b));
 
-  return (Math.random() * (max - min) + min).toFixed(decimal);
+  return +(Math.random() * (max - min) + min).toFixed(decimal);
 };
 
-export {getRandomFloat, getRandomInteger};
+const disableElement = (element) => {
+  element.toggleAttribute('disabled', true);
+};
+
+const enableElement = (element) => {
+  element.toggleAttribute('disabled', false);
+};
+
+const disableWindowScroll = () => {
+  winX = window.scrollX;
+  winY = window.scrollY;
+};
+
+const enableWindowScroll = () => {
+  winX = -1;
+  winY = -1;
+};
+
+export {winX, winY, getRandomFloat, getRandomInteger, disableElement, enableElement, disableWindowScroll, enableWindowScroll};
+
