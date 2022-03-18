2d82cc1e52c944c27b7bac4e1243a1b59f85e839
triple-sun <sploit.strannik@gmail.com>
2022-03-18 11:46:20 +0300

    Fixed methods and constants

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,12 +1,13 @@
-const offerSection = document.getElementsByClassName('notice')[0];
-const offerForm = offerSection.getElementsByClassName('ad-form')[0];
-const typeMenu = document.getElementById('type');
-const priceForm = document.getElementById('price');
-const roomNumber = document.getElementById('room_number');
-const capacity = document.getElementById('capacity');
+const offerSection = document.querySelector('.notice');
+const offerForm = offerSection.querySelector('.ad-form');
+const typeMenu = offerForm.querySelector('#type');
+const priceForm = offerForm.querySelector('#price');
+const roomNumber = offerForm.querySelector('#room_number');
+const capacity = offerForm.querySelector('#capacity');
 const pristine = new Pristine(offerForm);
+const MAX_ROOMS = 100;
 
-const MIN_PRICES = {
+const minPrices = {
   bungalow: 0,
   flat: 1000,
   hotel: 3000,
@@ -15,16 +16,17 @@ const MIN_PRICES = {
 };
 
 pristine.addValidator(capacity, () => {
-  if (roomNumber.value > capacity.value && roomNumber.value < 100 && capacity.value !== 0 || roomNumber.value >= 100 && capacity.value === 0) {
+  if (roomNumber.value > capacity.value && roomNumber.value < MAX_ROOMS && capacity.value !== '0' || roomNumber.value >= MAX_ROOMS && capacity.value === '0') {
     return true;
   }
   return false;
 }, 'Количество гостей должно соответствовать количеству комнат!');
 
+
 const validateForm = () => {
   typeMenu.addEventListener('change', () => {
-    priceForm.min = MIN_PRICES[typeMenu.value];
-    priceForm.placeholder = MIN_PRICES[typeMenu.value];
+    priceForm.min = minPrices[typeMenu.value];
+    priceForm.placeholder = minPrices[typeMenu.value];
   });
 
   offerForm.addEventListener('submit', (evt) => {
