0b2cb03ea491381f1062a2dcf439c07b5a422d41
triple-sun <sploit.strannik@gmail.com>
2022-03-18 11:55:49 +0300

    Fixed methods and constants

--- js/main.js
+++ js/main.js
@@ -3,8 +3,6 @@ import {disablePage, enablePage} from './form.js';
 import {validateForm} from './form-validation.js';
 
 
-//Скрыл элемент прошлого дз
-cardsListFragment.children[0].classList.add('hidden');
 document.querySelector('#map-canvas').appendChild(cardsListFragment.children[0]);
 
 disablePage();
