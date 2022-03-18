2ec2dd31c1f96046e326f962fae98ddd54ae2110
triple-sun <sploit.strannik@gmail.com>
2022-03-18 04:22:21 +0300

    Cleaned up code

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ import {cardsListFragment} from './cards.js';
 import {disablePage, enablePage} from './form.js';
 import {validateForm} from './form-validation.js';
 
-
-//Скрыл элемент прошлого дз
 document.getElementById('map-canvas').appendChild(cardsListFragment.children[0]);
 
 disablePage();
