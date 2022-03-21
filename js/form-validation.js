c0f8616398023150ae87e74161082263cca32374
triple-sun <sploit.strannik@gmail.com>
2022-03-21 17:27:17 +0300

    Fixed validation

--- js/form-validation.js
+++ js/form-validation.js
@@ -4,9 +4,7 @@ const typeMenu = offerForm.querySelector('#type');
 const priceForm = offerForm.querySelector('#price');
 const roomNumber = offerForm.querySelector('#room_number');
 const capacity = offerForm.querySelector('#capacity');
-const pristine = new Pristine(offerForm);
 const MAX_ROOMS = 100;
-
 const minPrices = {
   bungalow: 0,
   flat: 1000,
@@ -15,23 +13,49 @@ const minPrices = {
   palace: 10000
 };
 
-pristine.addValidator(capacity, () => {
-  if (roomNumber.value >= capacity.value && roomNumber.value < MAX_ROOMS && capacity.value !== '0' || roomNumber.value >= MAX_ROOMS && capacity.value === '0') {
+const pristine = new Pristine(offerForm, {
+  classTo: 'ad-form__element',
+  errorTextParent: 'ad-form__element',
+  errorTextTag: 'p',
+  errorTextClass: 'ad-form__error'
+}, true);
+
+function getPriceErrorMessage () {
+  return `Для данного типа жилья минимальная стоимость ${  minPrices[typeMenu.value]}`;
+}
+
+function getCapacityErrorMessage () {
+  if (Number(roomNumber.value) === MAX_ROOMS) {
+    return 'Выберите "не для гостей"';
+  }
+  return `Не больше ${  roomNumber.value  } гостей`;
+}
+
+function validateCapacity () {
+  if (roomNumber.value >= capacity.value && Number(roomNumber.value) < MAX_ROOMS && Number(capacity.value) !== 0 || Number(roomNumber.value) === MAX_ROOMS && Number(capacity.value) === 0) {
     return true;
   }
   return false;
-}, 'Количество гостей должно соответствовать количеству комнат!');
+}
 
+function validatePrice () {
+  if (priceForm.value < minPrices[typeMenu.value]) {
+    return false;
+  }
+  return true;
+}
 
-const validateForm = () => {
-  typeMenu.addEventListener('change', () => {
-    priceForm.min = minPrices[typeMenu.value];
-    priceForm.placeholder = minPrices[typeMenu.value];
-  });
+pristine.addValidator(priceForm, validatePrice, getPriceErrorMessage, 2, true);
+pristine.addValidator(capacity, validateCapacity, 'Количество гостей должно соответствовать количеству комнат', 2, true);
+pristine.addValidator(roomNumber, validateCapacity, getCapacityErrorMessage, 2, true);
 
-  offerForm.addEventListener('submit', () => {
-    pristine.validate();
-  });
-};
+typeMenu.addEventListener('change', () => {
+  priceForm.min = minPrices[typeMenu.value];
+  priceForm.placeholder = minPrices[typeMenu.value];
+});
 
-export { validateForm };
+offerForm.addEventListener('submit', (evt) => {
+  if (!pristine.validate()) {
+    evt.preventDefault();
+  }
+});
